93c557ad607ffa6f81a8288ccd7cdcc74e72c99f
flarmn <flarmn@gmail.com>
2023-03-03 21:08:41 +0300

    Adding cart component with cart icon outputed to page + some styling

---
+++ src/components/Cart.jsx
@@ -0,0 +1,14 @@
+function Cart(props){
+    console.log("Cart started");
+    console.log("props from Cart", props);
+
+    const {quantity = 0} = props
+
+    return  <div className="cart blue darken-4 white-text">
+        <i className = "material-icons">shoping_cart</i>
+        {quantity ? <span className = "cart-quantity">{quantity}</span> : null }
+    </div>
+
+}
+
+export {Cart}
